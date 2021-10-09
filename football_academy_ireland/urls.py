17c63c417bb8cfa411a959e50808fe95942c6f1c
robertc181 <rdancole@gmail.com>
2021-10-09 16:49:43 +0000

    Setup allauth

--- football_academy_ireland/urls.py
+++ football_academy_ireland/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from home.views import get_home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', get_home, name='get_home')
+    path('', get_home, name='get_home'),
+    path('accounts/', include('allauth.urls')),
 ]
