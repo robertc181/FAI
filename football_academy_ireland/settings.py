886452223aa8b7758670161886324c102d44724d
robertc181 <rdancole@gmail.com>
2021-10-25 11:58:31 +0000

    Add json and add training to the admin

--- football_academy_ireland/settings.py
+++ football_academy_ireland/settings.py
@@ -101,7 +101,8 @@ ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/training'
+LOGOUT_REDIRECT_URL = '/training'
 
 WSGI_APPLICATION = 'football_academy_ireland.wsgi.application'
 
--- training/models.py
+++ training/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Session(models.Model):
+    day = models.CharField(max_length=254)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    time = models.DecimalField(max_digits=6, decimal_places=2)
+    attendees =  models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.name
