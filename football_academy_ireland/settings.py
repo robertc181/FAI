40a73bd89c6d82ff475b21d931039618a5b6146a
robertc181 <rdancole@gmail.com>
2021-11-22 19:34:11 +0000

    Remove secret key and set debug

--- football_academy_ireland/settings.py
+++ football_academy_ireland/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pzu@x*eloql^&jubxn$6-j+)v3vz7#zary+adx=gecc4k)nrbk'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = "DEVELOPMENT" in os.environ
 
 ALLOWED_HOSTS = ['football-academy-ireland.herokuapp.com', 'localhost']
 
