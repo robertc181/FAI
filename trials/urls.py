92816db7fa3e56c6fc8b916e1b76f4fd3b310e7f
robertc181 <rdancole@gmail.com>
2021-11-23 17:17:48 +0000

    Add views urls and form.py for trial management in the trial app

--- trials/urls.py
+++ trials/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.trials, name='trials'),
     path('<trial_id>', views.trial_detail, name='trial_detail'),
+    path('add/', views.add_trial, name='add_trial'),
+    path('edit/<int:trial_id>', views.edit_trial, name='edit_trial'),
+    path('delete/<int:trial_id>', views.delete_trial, name='delete_trial'),
     
 ]
--- trials/views.py
+++ trials/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, reverse, redirect, get_object_or_404
 from .models import Trial
+from .forms import TrialForm
+from django.contrib import messages
 
 # Create your views here.
 
@@ -28,3 +30,63 @@ def trial_detail(request, trial_id):
 
     return render(request, 'trials/trial_details.html', context)
 
+
+def add_trial(request):
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store admin can do that')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        form = TrialForm(request.POST, request.FILES)
+        if form.is_valid():
+            trial = form.save()
+            messages.success(request, 'Successfully added a trial!')
+            return redirect(reverse('trial_detail', args=[trial.id]))
+        else:
+            messages.error(request, 'Failed to add trial. Please check the details and try again')
+    else: 
+        form = TrialForm()
+    template = 'trials/add_trial.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
+
+
+def delete_trial(request, trial_id):
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store admin can do that')
+        return redirect(reverse('home'))
+
+    trial = get_object_or_404(Trial, pk=trial_id)
+    trial.delete()
+    messages.success(request, 'trial deleted')
+    return redirect(reverse('trials'))
+
+
+def edit_trial(request, trial_id):
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store admin can do that')
+        return redirect(reverse('home'))
+
+    trial = get_object_or_404(Trial, pk=trial_id)
+    if request.method == 'POST':
+        form = TrialForm(request.POST, request.FILES, instance=trial)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated the trial!')
+            return redirect(reverse('trial_detail', args=[trial.id]))
+        else:
+            messages.error(request, 'Failed to update the trial. Please check the details and try again')
+    else:
+        form = TrialForm(instance=trial)
+        messages.info(request, 'You are editing this trial')
+
+    template = 'trials/edit_trial.html'
+    context = {
+        'form': form,
+        'trial': trial,
+    }
+
+    return render(request, template, context)
