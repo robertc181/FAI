77f18877737a75acda25bc774fe0c5707f00d8e0
robertc181 <rdancole@gmail.com>
2021-11-25 14:08:39 +0000

    Modify views.py in the home app to create a superuser in the deployed version of site

--- home/views.py
+++ home/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def get_home(request):
     """ A view to return the index page """
+
+    if not User.objects.filter(is_superuser=True).first():
+        user = User.objects.create(
+            username = 'admin',
+            email = 'admin@mywebsite.com',
+            is_superuser = True,
+        )
+        user.set_password('some password')
+        user.save()
 
     return render(request, 'home/index.html')
